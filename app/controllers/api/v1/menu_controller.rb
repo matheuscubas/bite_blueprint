ba2538ff5085e71c7c73ce4098d9eed3beb48a5c
mcubas <86804637+matheuscubas@users.noreply.github.com>
2025-03-30 13:16:20 -0300

    ♻️ Remove formatting logic from the controller

--- app/controllers/api/v1/menu_controller.rb
+++ app/controllers/api/v1/menu_controller.rb
@@ -5,7 +5,7 @@ module Api
         menus = Menu.includes(:menu_items).all
 
         if menus.any?
-          render json: menus_with_menu_items(menus:), status: :ok
+          render json: MenuRepresenter.for_collection.new(menus), status: :ok
         else
           render json: { error: "No menu found please try again later" }, status: :not_found
         end
@@ -15,40 +15,10 @@ module Api
         menu = Menu.includes(:menu_items).find_by(id: params[:id])
 
         if menu
-          render json: menu_representer(menu:), status: :ok
+          render json: MenuRepresenter.new(menu), status: :ok
         else
           render json: { error: "No menu found please try again later" }, status: :not_found
         end
-      end
-
-      private
-
-      def menus_with_menu_items(menus:)
-        return { menus: } if menus.empty?
-
-        menus.map do |menu|
-          result = menu_representer(menu:)
-
-          result
-        end
-      end
-
-      def menu_representer(menu:)
-        result = menu.attributes.symbolize_keys
-        result[:created_at] = result[:created_at].strftime("%Y-%m-%d")
-        result[:updated_at] = result[:updated_at].strftime("%Y-%m-%d")
-        result[:menu_items] = menu.menu_items.map { |menu_item| menu_item_representer(menu_item:) }
-
-        result
-      end
-
-      def menu_item_representer(menu_item:)
-        result = menu_item.attributes.symbolize_keys
-        result[:created_at] = result[:created_at].strftime("%Y-%m-%d")
-        result[:updated_at] = result[:updated_at].strftime("%Y-%m-%d")
-        result[:price] = result[:price] / 100.0
-
-        result.except(:menu_id)
       end
     end
   end
