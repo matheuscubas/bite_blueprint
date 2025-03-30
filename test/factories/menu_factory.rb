93d4a64b0e5604cda6893a6a6ec1e9374f3ed90e
mcubas <86804637+matheuscubas@users.noreply.github.com>
2025-03-30 12:14:37 -0300

    ♻️ Update factories to reflect the new relations between menu and menu_item

--- test/factories/menu_factory.rb
+++ test/factories/menu_factory.rb
@@ -4,7 +4,8 @@ FactoryBot.define do
   factory :menu do
     name { "#{Faker::Restaurant.name} - #{Faker::Food.ethnic_category}" }
     description  { Faker::Food.description }
+    association :restaurant, factory: :restaurant
 
-    after(:create) { |menu| FactoryBot.create_list(:menu_item, 5, menu: menu) }
+    after(:create) { |menu| FactoryBot.create_list(:menu_item, 5, menus: [ menu ]) }
   end
 end
