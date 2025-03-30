371794893e7e2b56885b849cb323f1982f8a2666
mcubas <86804637+matheuscubas@users.noreply.github.com>
2025-03-30 13:16:58 -0300

    ♻️ Update RestaurantController to use decorator and add test suit

--- test/controllers/api/v1/restaurant_controller_test.rb
+++ test/controllers/api/v1/restaurant_controller_test.rb
@@ -1,13 +1,28 @@
 require "test_helper"
 
-class Api::V1::RestaurantControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_v1_restaurant_index_url
-    assert_response :success
-  end
+module Api
+  module V1
+    class RestaurantControllerTest < ActionDispatch::IntegrationTest
+      test "Should return a list of restaurants" do
+        create_list(:restaurant, 5)
 
-  test "should get show" do
-    get api_v1_restaurant_show_url
-    assert_response :success
+        get api_v1_restaurant_index_url
+        body = JSON.parse(@response.body, symbolize_names: true)
+
+        assert body.size == 5
+        assert body.first[:name] == Restaurant.first.name
+        assert_response :success
+      end
+
+      test "Should return a specific restaurant" do
+        create(:restaurant)
+
+        get  api_v1_restaurant_url(1)
+        body = JSON.parse(@response.body, symbolize_names: true)
+
+        assert body[:name] == Restaurant.first.name
+        assert_response :success
+      end
+    end
   end
 end
