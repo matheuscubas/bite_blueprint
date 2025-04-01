5ed8373b200844dcccb4e839b0510a59037d365f
mcubas <86804637+matheuscubas@users.noreply.github.com>
2025-04-01 19:36:30 -0300

    ✨ Add nested_attributes and validation in models

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,9 +1,13 @@
 class Menu < ApplicationRecord
+  # associations
   belongs_to :restaurant
-
   has_and_belongs_to_many :menu_items
-  validates_associated :menu_items
 
+  # validations
   validates :name, presence: true
   validates_uniqueness_of :name, case_sensitive: false, scope: :restaurant_id
+
+  # nested_attributes
+  validates_associated :menu_items
+  accepts_nested_attributes_for :menu_items
 end
